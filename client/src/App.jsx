5b4f9d51ab5ee173b90ecf9f8cfd6d1b9bc6eb0a
othman <oteman.raja@gmail.com>
2024-04-21 15:20:42 +0100

    create app react

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+function App() {
+  
+
+  return (
+   <h1 className=''>Hello</h1>
+  )
+}
+
+export default App
