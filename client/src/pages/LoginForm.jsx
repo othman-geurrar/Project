207d1da77b7e730e1f0f5414f8d569b55c4f97a1
soukainaj <soukainagoumih7@gmail.com>
2024-04-22 16:40:12 +0100

    sk

--- client/src/pages/LoginForm.jsx
+++ client/src/pages/LoginForm.jsx
@@ -1,9 +1,124 @@
-import React from 'react'
+import React from "react";
+import Logo from "../assets/img/logo.png";
+import NavBar from "../components/NavBar";
 
 const LoginForm = () => {
   return (
-    <div>LoginForm</div>
-  )
-}
+    <>
+      <NavBar />
+      <section className="h-full bg-neutral-200 dark:bg-neutral-700">
+        <div className="container h-full p-10">
+          <div className="g-6 flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
+            <div className="w-full">
+              <div className="block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
+                <div className="g-0 lg:flex lg:flex-wrap">
+                  {/* <!-- Left column container--> */}
+                  <div className="px-4 md:px-0 lg:w-6/12">
+                    <div className="flex min-h-full flex-col justify-center px-6 py-16 lg:px-8">
+                      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                        <img
+                          className="mx-auto h-24 w-auto mt-0"
+                          src={Logo}
+                          alt="Your Company"
+                        />
+                        <h2 className="mt-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                          Sign in to your account
+                        </h2>
+                      </div>
 
-export default LoginForm
+                      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                        <form className="space-y-6" action="#" method="POST">
+                          <div>
+                            <label
+                              htmlFor="email"
+                              className="block text-sm font-medium leading-6 text-gray-900"
+                            >
+                              Email address
+                            </label>
+                            <div className="mt-2">
+                              <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
+                              />
+                            </div>
+                          </div>
+
+                          <div>
+                            <div className="flex items-center justify-between">
+                              <label
+                                htmlFor="password"
+                                className="block text-sm font-medium leading-6 text-gray-900"
+                              >
+                                Password
+                              </label>
+                              <div className="text-sm">
+                                <a
+                                  href="#"
+                                  className="font-semibold text-teal-600 hover:text-teal-500"
+                                >
+                                  Forgot password?
+                                </a>
+                              </div>
+                            </div>
+                            <div className="mt-2">
+                              <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
+                              />
+                            </div>
+                          </div>
+
+                          <div>
+                            <button
+                              type="submit"
+                              className="flex w-full mb-4 justify-center rounded-md bg-teal-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
+                            >
+                              Sign in
+                            </button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* <!-- Right column container with background and description--> */}
+                  <div
+                    className="flex items-center rounded-b-lg lg:w-6/12 lg:rounded-r-lg lg:rounded-bl-none"
+                    style={{
+                      background:
+                        // "linear-gradient(to right, #009079, #00A98F, #00C3A5, #00DCBA)",
+                        "linear-gradient(to right, #00DCBA, #00C3A5, #00A98F, #009079)",
+                    }}
+                  >
+                    <div className="px-4 py-6 text-white md:mx-6 md:p-12">
+                      <h4 className="mb-6 text-xl font-semibold">
+                        We are more than just a website
+                      </h4>
+                      <p className="text-md">
+                        Welcome to OSAY, your lifestyle hub for personalized
+                        expression. Dive into our curated selections designed to
+                        complement your individual tastes and interests. From
+                        fashion to wellness to adventure, find everything you
+                        need to enhance your lifestyle. Explore expertly crafted
+                        content that inspires and informs. Embrace your unique
+                        journey with confidence, only at OSAY.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default LoginForm;
--- client/src/pages/RegisterForm.jsx
+++ client/src/pages/RegisterForm.jsx
@@ -1,9 +1,101 @@
-import React from 'react'
+import React from "react";
+import Logo from "../assets/img/logo.png";
 
 const RegisterForm = () => {
   return (
-    <div>RegisterForm</div>
-  )
-}
+    <>
+      <div className="px-4 md:px-0 ">
+        <div className="flex min-h-full flex-col justify-center px-6 py-16 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img
+              className="mx-auto h-24 w-auto mt-0"
+              src={Logo}
+              alt="Your Company"
+            />
+            <h2 className="mt-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Sign Up
+            </h2>
+          </div>
 
-export default RegisterForm
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" action="#" method="POST">
+            <div>
+                <label
+                  htmlFor="Username"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Username
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="Username"
+                    name="Username"
+                    type="Username"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Password
+                  </label>
+                  <div className="text-sm">
+                    <a
+                      href="#"
+                      className="font-semibold text-teal-600 hover:text-teal-500"
+                    >
+                      Forgot password?
+                    </a>
+                  </div>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full mb-4 justify-center rounded-md bg-teal-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
+                >
+                  Sign in
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RegisterForm;
