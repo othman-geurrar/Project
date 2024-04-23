a4ddb4dd9912eaa19c0df472b533e9effe54ffc7
othman <oteman.raja@gmail.com>
2024-04-23 21:12:33 +0100

    side bar

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,9 +1,164 @@
-import React from 'react'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { SiShopware } from "react-icons/si";
+import { MdOutlineCancel } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { IoMdContacts } from 'react-icons/io';
+import { RiContactsLine, RiStockLine } from 'react-icons/ri';
+import { GiLouvrePyramid } from 'react-icons/gi';
+import { BsKanban, BsBarChart, BsBoxSeam, BsCurrencyDollar, BsShield, BsChatLeft } from 'react-icons/bs';
+import { AiOutlineCalendar, AiOutlineShoppingCart, AiOutlineAreaChart, AiOutlineBarChart, AiOutlineStock } from 'react-icons/ai';
+import {
+  FiShoppingBag,
+  FiEdit,
+  FiPieChart,
+  FiBarChart,
+  FiCreditCard,
+  FiStar,
+  FiShoppingCart,
+} from "react-icons/fi";
+
+const links = [
+  {
+    title: "Dashboard",
+    links: [
+      {
+        name: "ecommerce",
+        icon: <FiShoppingBag />,
+      },
+    ],
+  },
+
+  {
+    title: "Pages",
+    links: [
+      {
+        name: "Orders",
+        icon: <AiOutlineShoppingCart />,
+      },
+      {
+        name: "Admins",
+        icon: <IoMdContacts />,
+      },
+      {
+        name: "Users",
+        icon: <RiContactsLine />,
+      },
+    ],
+  },
+  {
+    title: "Apps",
+    links: [
+      {
+        name: "calendar",
+        icon: <AiOutlineCalendar />,
+      },
+      {
+        name: "kanban",
+        icon: <BsKanban />,
+      },
+      {
+        name: "editor",
+        icon: <FiEdit />,
+      },
+      
+    ],
+  },
+  {
+    title: "Charts",
+    links: [
+      {
+        name: "line",
+        icon: <AiOutlineStock />,
+      },
+      {
+        name: "area",
+        icon: <AiOutlineAreaChart />,
+      },
+
+      {
+        name: "bar",
+        icon: <AiOutlineBarChart />,
+      },
+      {
+        name: "pie",
+        icon: <FiPieChart />,
+      },
+      {
+        name: "financial",
+        icon: <RiStockLine />,
+      },
+      {
+        name: "color-mapping",
+        icon: <BsBarChart />,
+      },
+      {
+        name: "pyramid",
+        icon: <GiLouvrePyramid />,
+      },
+      {
+        name: "stacked",
+        icon: <AiOutlineBarChart />,
+      },
+    ],
+  },
+];
+
+
 
 const SideBar = () => {
-  return (
-    <div>SideBar</div>
-  )
-}
+  const activeMenu = true;
 
-export default SideBar
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
+  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
+
+  return (
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+      {activeMenu && (
+        <>
+          <div className="flex justify-between items-center">
+            <Link
+              to="/"
+              onClick={() => {}}
+              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-teal-600"
+            >
+              <SiShopware /> <span>OSAY</span>
+            </Link>
+            <TooltipComponent content="Menu" position="BottomCenter">
+              <button
+                type="button"
+                onClick={() => {}}
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+              >
+                <MdOutlineCancel />
+              </button>
+            </TooltipComponent>
+          </div>
+          <div className="mt-10">
+            {links.map((item) => (
+              <div key={item.title}>
+                <p className="text-gray-400 dark:text-gray-400 m-3 mt-4 uppercase">
+                  {item.title}
+                </p>
+                {item.links.map((link) => (
+                  <NavLink
+                    to={`/${link.name}`}
+                    key={link.name}
+                    onClick={()=>{}}
+                    
+                    className={({ isActive }) => (isActive ? activeLink : normalLink)}
+                  >
+                    {link.icon}
+                    <span className="capitalize ">{link.name}</span>
+                  </NavLink>
+                ))}
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default SideBar;
