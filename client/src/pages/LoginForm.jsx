f1abcf1d022d9866e383c6630e2cf9176e9b5607
othman <oteman.raja@gmail.com>
2024-04-23 09:07:55 +0100

    login and register

--- client/src/pages/LoginForm.jsx
+++ client/src/pages/LoginForm.jsx
@@ -1,9 +1,109 @@
-import React from 'react'
+import React from "react";
+import Logo from "../assets/img/logo.png";
+import NavBar from "../components/NavBar";
+import { TextBoxComponent } from '@syncfusion/ej2-react-inputs';
 
 const LoginForm = () => {
   return (
-    <div>LoginForm</div>
-  )
-}
+    <>
+      <NavBar />
+      <section className="h-full bg-neutral-200 dark:bg-neutral-700">
+        <div className="container h-full p-10">
+          <div className="g-6 flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
+            <div className="w-full">
+              <div className="block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
+                <div className="g-0 lg:flex lg:flex-wrap">
+                  {/* <!-- Left column container--> */}
+                  <div className="px-2 md:px-0 lg:w-6/12">
+                    <div className="flex min-h-full flex-col justify-center px-6 py-16 lg:px-8">
+                      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                        <img
+                          className="mx-auto h-24 w-auto mt-0"
+                          src={Logo}
+                          alt="Your Company"
+                        />
+                        <h2 className="mt-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                          Sign in to your account
+                        </h2>
+                      </div>
 
-export default LoginForm
+                      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                        <form className="space-y-6" action="#" method="POST">
+                        
+
+                          <div>
+                            <div className="row custom-margin custom-padding-5 material">
+                              <div className="col-xs-6 col-sm-6 col-lg-6 col-md-6 mb-5">
+                                <TextBoxComponent
+                                  placeholder="Email Address"
+                                  cssClass="e-outline"
+                                  floatLabelType="Auto"
+                                />
+                              </div>
+                              <div className="col-xs-6 col-sm-6 col-lg-6 col-md-6 mb-5">
+                                <TextBoxComponent
+                                  placeholder="Password"
+                                  cssClass="e-outline"
+                                  floatLabelType="Auto"
+                                />
+                              </div>
+                              <div className="text-sm">
+                                <a
+                                  href="#"
+                                  className="font-semibold text-teal-600 hover:text-teal-500"
+                                >
+                                  Forgot password?
+                                </a>
+                              </div>
+                              
+                            </div>
+                          </div>
+
+                          <div>
+                            <button
+                              type="submit"
+                              className="flex w-full mb-4 justify-center rounded-md bg-teal-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
+                            >
+                              Sign in
+                            </button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* <!-- Right column container with background and description--> */}
+                  <div
+                    className="flex items-center rounded-b-lg lg:w-6/12 lg:rounded-r-lg lg:rounded-bl-none"
+                    style={{
+                      background:
+                        // "linear-gradient(to right, #009079, #00A98F, #00C3A5, #00DCBA)",
+                        "linear-gradient(to right, #00DCBA, #00C3A5, #00A98F, #009079)",
+                    }}
+                  >
+                    <div className="px-4 py-6 text-white md:mx-6 md:p-12">
+                      <h4 className="mb-6 text-xl font-semibold">
+                        We are more than just a website
+                      </h4>
+                      <p className="text-md">
+                        Welcome to OSAY, your lifestyle hub for personalized
+                        expression. Dive into our curated selections designed to
+                        complement your individual tastes and interests. From
+                        fashion to wellness to adventure, find everything you
+                        need to enhance your lifestyle. Explore expertly crafted
+                        content that inspires and informs. Embrace your unique
+                        journey with confidence, only at OSAY.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default LoginForm;
--- client/src/pages/RegisterForm.jsx
+++ client/src/pages/RegisterForm.jsx
@@ -1,9 +1,102 @@
-import React from 'react'
+import React from "react";
+import NavBar from "../components/NavBar";
+import { TextBoxComponent } from '@syncfusion/ej2-react-inputs';
+import logo from '../assets/img/logo2.png';
 
 const RegisterForm = () => {
   return (
-    <div>RegisterForm</div>
-  )
-}
+    <>
+      <NavBar />
+      <div className="h-full bg-neutral-200 dark:bg-neutral-700">
+        <div className="mx-auto">
+          <div className="flex justify-center px-6 py-12">
+            <div className="w-full xl:w-3/4 lg:w-11/12 flex">
+              <div
+                className="w-full h-auto lg:block lg:w-5/12 bg-cover rounded-l-lg"
+                style={{
+                  background:
+                    // "linear-gradient(to right, #009079, #00A98F, #00C3A5, #00DCBA)",
+                    "linear-gradient(to right, #00DCBA, #00C3A5, #00A98F, #009079)",
+                }}
+              >
+                <img
+                  className="mt-28 object-cover h-72 opacity-100 "
+                  src={logo}
+                  alt="logo"
+                />
+              </div>
 
-export default RegisterForm
+              <div className="w-full lg:w-7/12 bg-white dark:bg-gray-700 p-5 rounded-lg lg:rounded-l-none">
+                <h3 className="py-4 text-2xl text-center text-gray-800 dark:text-white">
+                  Create an Account!
+                </h3>
+                <form className="px-8 pt-6 pb-8 mb-4 bg-white dark:bg-gray-800 rounded">
+                  <div className="mb-4 md:flex ">
+                  <div className="col-xs-6 col-sm-6 col-lg-6 col-md-6 mb-5 mr-16">
+                  <label className="block mb-2 text-sm font-bold text-gray-700 dark:text-white" htmlFor="firstName">
+                                    First Name
+                    </label>
+                      <TextBoxComponent
+                        placeholder="First Name"
+                        cssClass="e-outline"
+                        floatLabelType="Auto"
+                      />
+                    </div>
+                    <div className="col-xs-6 col-sm-6 col-lg-6 col-md-6 mb-5">
+                    <label className="block mb-2 text-sm font-bold text-gray-700 dark:text-white" htmlFor="lastName">
+                                    Last Name
+                    </label>
+                      <TextBoxComponent
+                        placeholder="Last name"
+                        cssClass="e-outline"
+                        floatLabelType="Auto"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-xs-6 col-sm-6 col-lg-6 col-md-6 mb-5">
+                  <label className="block mb-2 text-sm font-bold text-gray-700 dark:text-white" htmlFor="email">
+                                    Email
+                    </label>
+                    <TextBoxComponent
+                      placeholder="Email Address"
+                      cssClass="e-outline"
+                      floatLabelType="Auto"
+                      id="email"
+                      name="email"
+                      type="email"
+                      required
+                    />
+                  </div>
+                  <div className="mb-4 ">
+                    <div className="col-xs-6 col-sm-6 col-lg-6 col-md-6 mb-5">
+                    <label className="block mb-2 text-sm font-bold text-gray-700 dark:text-white" htmlFor="password">
+                                    Password
+                    </label>
+                      <TextBoxComponent
+                        placeholder="Password"
+                        cssClass="e-outline"
+                        floatLabelType="Auto"
+                        type="password"
+                      />
+                    </div>
+                    
+                  </div>
+                  <div className="mb-6 text-center">
+                    <button
+                      className="w-full px-4 py-2 font-bold text-white bg-teal-600 rounded-full hover:bg-teal-500 dark:bg-blue-700 dark:text-white dark:hover:bg-blue-900 focus:outline-none focus:shadow-outline"
+                      type="button"
+                    >
+                      Register Account
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RegisterForm;
