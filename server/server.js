246808b4b52122ddbb5258e9e9b3148a83fe2a6f
othman geurrar <147775783+othman-geurrar@users.noreply.github.com>
2024-07-12 15:12:14 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://osay.vercel.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
